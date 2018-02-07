7affbaa6ceb81efca0dbb9f2712ceb4229696556
cccchh <cc@cc.com>
2018-02-07 12:26:44 +0900

    [추장훈] React 성능측정

--- ch06/src/index.js
+++ ch06/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 // import App from './portal/App';
-import AppContainer from './todolist/components/AppContainer';
+// import AppContainer from './todolist/components/AppContainer';
+import App from './perftest/App';
 import './index.css';
+import registerObserver from 'react-perf-devtool'
 import registerServiceWorker from './registerServiceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<AppContainer />, document.getElementById('root'));
+registerObserver();
+ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<AppContainer />, document.getElementById('root'));
 registerServiceWorker();
